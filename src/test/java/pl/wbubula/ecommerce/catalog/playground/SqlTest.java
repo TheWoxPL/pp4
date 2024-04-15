60015a0e0cc63bf1b779ee09c26eaeb9bc5a56cd
TheWoxPL <wojtek.bubula@onet.pl>
2024-04-15 16:30:48 +0200

    Add simple h2 sql db

---
+++ src/test/java/pl/wbubula/ecommerce/catalog/playground/SqlTest.java
@@ -0,0 +1,32 @@
+package pl.wbubula.ecommerce.catalog.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class SqlTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Test
+    void itSelectForCurrentDate() {
+        var myDate=jdbcTemplate.queryForObject(
+                "SElect now() myCurrentDate",
+                String.class
+        );
+
+        assertThat(myDate).contains("2024");
+    }
+
+    @Test
+    void iAmAbleToCreateTable() {
+       var sql = "Create table xyz ()";
+
+//        assertThat(myDate).contains("2024");
+    }
+
+}
