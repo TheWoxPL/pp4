dd99cf27b859938a98c86d33e16b35d43ec5f48d
s-301-26 <s-301-26@gumiforest.uek.krakow.pl>
2024-04-29 16:31:45 +0200

    Add web interface

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,2 +1,31 @@
-const hello = () => alert("it works")
-hello()
+getProducts = () => {
+  return fetch("/api/products")
+  .then(response => response.json());
+}
+ 
+const createProductHtml = (productData) => {
+    const template = `
+        <div>
+            <h4>${productData.name}</h4>
+            <span>${productData.price}</span>
+            <img src='https://picsum/id/237/200/300'/>
+            <button data-id="${productData.id}">Add to cart</button>
+        </div>
+    `
+
+    const productEl = document.createElement('li');
+    productEl.innerHTML = "abc xyz";
+ 
+    return productEl;
+}
+ 
+document.addEventListener("DOMContentLoaded", () => {
+    const productsListEl = document.querySelector("#productsList");
+    getProducts()
+        .then(productsAsJsonObj => productsAsJsonObj.map(product => createProductHtml))
+        .then(productsAsHtmlEl => {
+            productsAsHtmlEl.forEach(productEl => productsListEl.appendChild(productEl));
+        })
+});
+
+console.log("asd")
